cf4ed91b2f760187af328b58301c6e8907944bd0
Samuel Hernandez <doicirohangnan@gmail.com>
2021-03-14 13:05:03 -0500

    3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        int m = 7;
+        for (ArrayList<Integer> ali: findPair(m,arrayList)) {
+            System.out.println(ali.toString());
+        }
+        System.out.println();
         br.close();
         bw.close();
+    }
+
+    public static ArrayList<ArrayList<Integer>> findPair(int m, ArrayList<Integer> arrayList) {
+        ArrayList<ArrayList<Integer>> pairList = new ArrayList<>();
+        for (int a: arrayList) {
+           ArrayList<Integer> pair = new ArrayList<>();
+            int b = 0;
+            try {
+                b = arrayList.get(binSearch(arrayList, (m - a)));
+            } catch (IndexOutOfBoundsException e) {
+                b = -1;
+            }
+           if (b != -1) {
+               pair.add(a);
+               pair.add(b);
+               pairList.add(pair);
+           }
+        }
+        return pairList;
     }
 
     public static int binSearch(ArrayList<Integer> arrayList, int x) {
